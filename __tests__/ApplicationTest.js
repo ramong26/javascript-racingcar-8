cca831641810985911c8f7b941582b32f4e8ed56
김수연 <rlatndus21049@gmail.com>
2025-10-25 14:43:41 +0900

    feat/전체적인 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,4 +1,4 @@
-import App from "../src/App.js";
+import App from '../src/App.js';
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 const mockQuestions = (inputs) => {
@@ -11,50 +11,95 @@ const mockQuestions = (inputs) => {
 };
 
 const mockRandoms = (numbers) => {
-  MissionUtils.Random.pickNumberInRange = jest.fn();
+  MissionUtils.Random.pickNumberInRange=jest.fn();
 
-  numbers.reduce((acc, number) => {
+  numbers.reduce((acc,number)=>{
     return acc.mockReturnValueOnce(number);
-  }, MissionUtils.Random.pickNumberInRange);
+  }, MissionUtils.Random.pickNumberInRange)
 };
 
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+const getLogSpy=()=>{
+  const logSpy = jest.spyOn(MissionUtils.Console,'print');
   logSpy.mockClear();
   return logSpy;
-};
+}
 
-describe("자동차 경주", () => {
-  test("기능 테스트", async () => {
-    // given
-    const MOVING_FORWARD = 4;
-    const STOP = 3;
-    const inputs = ["pobi,woni", "1"];
-    const logs = ["pobi : -", "woni : ", "최종 우승자 : pobi"];
-    const logSpy = getLogSpy();
-
+describe('자동차 경주',()=>{
+  test('기능 테스트', async()=>{
+    const inputs=['pobi,woni','1'];
     mockQuestions(inputs);
-    mockRandoms([MOVING_FORWARD, STOP]);
+    mockRandoms([3,4]);
 
-    // when
+    const logSpy=getLogSpy();
+    const outputs = ["실행 결과", "pobi : ", "woni : -", "최종 우승자 : woni"];
+
     const app = new App();
     await app.run();
 
-    // then
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
+    outputs.forEach((output)=>{
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output))
+    })
+  })
+
+  test('자동차 이동 조건 테스트', async () => {
+    const inputs = ["pobi", "3"];
+    mockQuestions(inputs);
+    mockRandoms([4, 3, 5]); 
+        
+    const logSpy = getLogSpy();
+    const app = new App();
+    await app.run();
+        
+    expect(logSpy).toHaveBeenCalledWith("pobi : -");
+    expect(logSpy).toHaveBeenCalledWith("pobi : -");
+    expect(logSpy).toHaveBeenCalledWith("pobi : --");
   });
 
-  test("예외 테스트", async () => {
-    // given
-    const inputs = ["pobi,javaji"];
+  test('공동 우승자 테스트', async()=>{
+    const inputs = ["pobi,woni,jun", "1"];
+    mockQuestions(inputs);
+    mockRandoms([4, 4, 3]);
+
+    const logSpy=getLogSpy();
+    const app = new App();
+    await app.run();
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("최종 우승자 : pobi, woni"));
+  });
+
+  test('자동차 이름 중복 예외 테스트', async()=>{
+    const inputs = ["pobi,pobi"];
     mockQuestions(inputs);
 
-    // when
     const app = new App();
 
-    // then
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+    await expect(app.run()).rejects.toThrow("[ERROR] 자동차 이름은 중복될 수 없습니다.");
   });
-});
+
+  test('자동차 이름 비어있을 경우 테스트', async()=>{
+    const inputs = ["pobi,"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR] 자동차 이름은 비어있을 수 없습니다.");
+  });
+
+  test('자동차 이름 입력하지 않을 경우 테스트', async()=>{
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR] 자동차 이름을 입력하지 않았습니다.");
+  });
+
+  test('시도 횟수를 입력하지 않을 경우 테스트', async()=>{
+    const inputs = ["pobi,woni", ""];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR] 시도할 횟수를 입력하지 않았습니다.");
+  });
+})
--- src/App.js
+++ src/App.js
@@ -1,5 +1,63 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    // 자동차 이름 입력 및 유효성 검사 로직
+    const carInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.\n');
+
+    if(!carInput){
+      throw new Error('[ERROR] 자동차 이름을 입력하지 않았습니다.')
+    }
+
+    const carList = carInput.split(',').map(name => name.trim());
+    const uniqueNames = new Set(carList);
+    
+    // 이름 중복 및 빈 이름 체크
+    if(uniqueNames.size !== carList.length){
+      throw new Error('[ERROR] 자동차 이름은 중복될 수 없습니다.')
+    }
+    if(carList.some(name => name.length === 0)){
+      throw new Error('[ERROR] 자동차 이름은 비어있을 수 없습니다.')
+    }
+
+    // 시도 횟수 입력 및 유효성 검사 로직
+    const tryAttempt = await Console.readLineAsync('시도할 횟수는 몇 회인가요? \n')
+
+    if(!tryAttempt){
+      throw new Error('[ERROR] 시도할 횟수를 입력하지 않았습니다.')
+    }
+
+   // 자동차 경주 게임 로직
+    let carPositions = new Array(carList.length).fill(0);
+
+    Console.print('실행 결과');
+
+    for(let i = 1; i<=tryAttempt; i++){
+      for(let j = 0; j<carList.length; j++){
+        let randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+       
+        if(randomNumber >= 4) carPositions[j]++;
+        
+        let moveDisplay = '-'.repeat(carPositions[j])
+        Console.print(`${carList[j]} : ${moveDisplay}`)
+      }
+      Console.print('');
+    }
+
+    // 최종 우승자 안내 문구 출력 로직
+    const carScore = carList.map((name, index) => ({
+      name: name,
+      position: carPositions[index]
+    }));
+
+    const maxPosition = Math.max(...carPositions);
+    const winners = carScore.filter(car => car.position === maxPosition);
+    const winnerNames = winners.map(winner => winner.name);
+    
+    Console.print(`최종 우승자 : ${winnerNames.join(', ')}`);
+  }
 }
 
 export default App;
+
